d9ceb767e2cdc5a03d8e6027097677fa59f72db2
Karan Shelar <karanshelar8775@gmail.com>
2026-02-14 18:51:00 +0530

    Fix syntax error in sqlalchemy import

--- backend/db/__init__.py
+++ backend/db/__init__.py
@@ -1,5 +1,6 @@
 """Database initialization module."""
-from sqlalchemy. orm import declarative_base
+from sqlalchemy.orm import declarative_base
+
 
 Base = declarative_base()
 metadata = Base.metadata
